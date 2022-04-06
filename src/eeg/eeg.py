e71b3aeb4587ab128ff4141c2d4e340b54216745
zeyus <zeyus@zeyus.com>
2022-04-06 09:28:23 +0200

    sanity wrapping for board commands

--- src/eeg/eeg.py
+++ src/eeg/eeg.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import time
 from typing import List, Tuple
 from nptyping import NDArray, Float64
@@ -9,14 +10,136 @@ from brainflow.data_filter import DataFilter, FilterTypes, DetrendOperations, No
 import pyxdf
 import pandas as pd
 
+class CytonSampleRate(Enum):
+  SR_250 = 6
+  SR_500 = 5
+  SR_1000 = 4
+  SR_2000 = 3
+  SR_4000 = 2
+  SR_8000 = 1
+  SR_16000 = 0
+
+class CytonInputType(Enum):
+  ADSINPUT_NORMAL = 0
+  ADSINPUT_SHORTED = 1
+  ADSINPUT_BIAS_MEAS = 2
+  ADSINPUT_MVDD = 3
+  ADSINPUT_TEMP = 4
+  ADSINPUT_TESTSIG = 5
+  ADSINPUT_BIAS_DRP = 6
+  ADSINPUT_BIAS_DRN = 7
+
+class CytonRecordingDuration(Enum):
+  MIN_5 = 'A'
+  MIN_15 = 'S'
+  MIN_30 = 'F'
+  MIN_60 = 'G'
+  MIN_120 = 'H'
+  MIN_240 = 'J'
+  MIN_720 = 'K'
+  MIN_1440 = 'L'
+  TEST_14_SEC = 'a'
+
+class CytonChannel(Enum):
+  CH_1 = 1
+  CH_2 = 2
+  CH_3 = 3
+  CH_4 = 4
+  CH_5 = 5
+  CH_6 = 6
+  CH_7 = 7
+  CH_8 = 8
+  CH_9 = 'Q'
+  CH_10 = 'W'
+  CH_11 = 'E'
+  CH_12 = 'R'
+  CH_13 = 'T'
+  CH_14 = 'Y'
+  CH_15 = 'U'
+  CH_16 = 'I'
+
+  @classmethod
+  def from_channel_number(cls, channel_number: int) -> 'CytonChannel':
+    return cls['CH_{}'.format(channel_number)]
+
+class CytonGain(Enum):
+  GAIN_1 = 0
+  GAIN_2 = 1
+  GAIN_4 = 2
+  GAIN_6 = 3
+  GAIN_8 = 4
+  GAIN_12 = 5
+  GAIN_24 = 6
+  
+class CytonCommands(Enum):
+  SAMPLE_RATE_PREFIX = '~'
+  BOARD_MODE_PREFIX = '/'
+  CHANNEL_CONFIG_PREFIX = 'x'
+  CHANNEL_CONFIG_SUFFIX = 'X'
+  RESET_CHANNELS = 'd'
+  SD_STOP = 'j'
+  STREAM_START = 'b'
+  STREAM_STOP = 's'
+  TIMESTAMP_START = '<'
+  TIMESTAMP_STOP = '>'
+  CHANNEL_1_ON = '!'
+  CHANNEL_1_OFF = '1'
+  CHANNEL_2_ON = '@'
+  CHANNEL_2_OFF = '2'
+  CHANNEL_3_ON = '#'
+  CHANNEL_3_OFF = '3'
+  CHANNEL_4_ON = '$'
+  CHANNEL_4_OFF = '4'
+  CHANNEL_5_ON = '%'
+  CHANNEL_5_OFF = '5'
+  CHANNEL_6_ON = '^'
+  CHANNEL_6_OFF = '6'
+  CHANNEL_7_ON = '&'
+  CHANNEL_7_OFF = '7'
+  CHANNEL_8_ON = '*'
+  CHANNEL_8_OFF = '8'
+  CHANNEL_9_ON = 'Q'
+  CHANNEL_9_OFF = 'q'
+  CHANNEL_10_ON = 'W'
+  CHANNEL_10_OFF = 'w'
+  CHANNEL_11_ON = 'E'
+  CHANNEL_11_OFF = 'e'
+  CHANNEL_12_ON = 'R'
+  CHANNEL_12_OFF = 'r'
+  CHANNEL_13_ON = 'T'
+  CHANNEL_13_OFF = 't'
+  CHANNEL_14_ON = 'Y'
+  CHANNEL_14_OFF = 'y'
+  CHANNEL_15_ON = 'U'
+  CHANNEL_15_OFF = 'u'
+  CHANNEL_16_ON = 'I'
+  CHANNEL_16_OFF = 'i'
+
+  @classmethod
+  def channel_number_on(cls, channel_number: int) -> 'CytonCommands':
+    return cls['CHANNEL_{}_ON'.format(channel_number)]
+  @classmethod
+  def channel_number_off(cls, channel_number: int) -> 'CytonCommands':
+    return cls['CHANNEL_{}_OFF'.format(channel_number)]
+
+class CytonBoardMode(Enum):
+  DEFAULT = 0
+  DEBUG = 1
+  ANALOG = 2
+  DIGITAL = 3
+  MARKER = 4
+  GET_BOARD_MODE = '/'
+
 class EEG(object):
-  def __init__(self, dummyBoard: bool = False) -> None:
+  def __init__(self, dummyBoard: bool = False, emg_channels: List[int] = []) -> None:
     self.params = BrainFlowInputParams()
     self.curves = []
+    self.dummyBoard = dummyBoard
     if(dummyBoard):
       self._prepare_dummy_board()
     else:
       self._prepare_board()
+    self.emg_channels = emg_channels
     self.exg_channels = BoardShim.get_exg_channels(self.board_id)
     self.accel_channels = BoardShim.get_accel_channels(self.board_id)
     self.sampling_rate = BoardShim.get_sampling_rate(self.board_id)
@@ -25,76 +148,109 @@ class EEG(object):
 
     # self.start_stream()
     
-  def start_stream(self) -> None:
+  def start_stream(self, sdcard = True, sr: CytonSampleRate = None, duration: CytonRecordingDuration = CytonRecordingDuration.MIN_120) -> None:
+    """
+    Starts recording EEG data either to sd card or via dongle
+    defaults to 120 minutes for SDCard recording
+    Sample rate defaults to 250Hz for streaming over dongle
+    and 1000Hz for recording sd card
+    """
+    # connect to board
     self.board.prepare_session()
-    self.board.start_stream()
+
+    # no channel config possible
+    if self.dummyBoard:
+      self.board.start_stream()
+      return
+    
+    # set channels to default
+    self._set_channels_to_defaults()
+    # set EMG configuration
+    self._config_emg_channels()
+
+    # start streaming
+    if not sdcard:
+      if sr is None:
+        sr = CytonSampleRate.SR_250
+      self._set_sample_rate(sr)
+      self.board.start_stream()
+      return
+    
+    # sdcard
+    if sr is None:
+      sr = CytonSampleRate.SR_1000
+      self._set_sample_rate(sr)
+
+    # if we get here it's a real board
+    # and sdcard is True
+    self._start_sd_recording(duration)
+  
+  def _config_emg_channels(self) -> bool:
+    """
+    Disable SRB1 and 2 for EMG channels
+    """
+    for channel in self.emg_channels:
+      ch = CytonChannel.from_channel_number(channel)
+      self._channel_config(ch, disable=False, gain=CytonGain.GAIN_24, input_type=0, bias=False, srb1=False, srb2=False)
+    return True
 
   def _send_command(self, command: str) -> None:
     response = self.board.config_board(command)
     return response
 
   def _set_channels_to_defaults(self) -> None:
-    self._send_command(b'd')
+    self._send_command(CytonCommands.RESET_CHANNELS.value)
 
   def _channel_config(self,
-                        channel: int,
+                        channel: CytonChannel,
                         disable: bool = False,
-                        gain: int = 6,
-                        input_type: int = 0,
+                        gain: CytonGain = CytonGain.GAIN_24,
+                        input_type: CytonInputType = CytonInputType.ADSINPUT_NORMAL,
                         bias: bool = True,
                         srb1: bool = False,
                         srb2: bool = True) -> bool:
     """
-    Change SRB1, SRB2, BIAS
-    NOTE: this will reset the channels amp settings
+    Configure a single channel's settings
+    Defaults:
+      disable = False
+      gain = GAIN_24
+      input_type = ADSINPUT_NORMAL
+      bias = True
+      srb1 = False
+      srb2 = True
     """
-    if channel < 1 or channel > self.exg_channels:
-      raise ValueError('Invalid channel number')
-    if gain < 0 or gain > 6:
-      raise ValueError('Invalid gain value')
-    if input_type < 0 or input_type > 7:
-      raise ValueError('Invalid input type')
 
-    self._send_command('x {channel} {disable} {gain} {input_type} {bias} {srb1} {srb2} X'.format(
-      channel = channel,
+    self._send_command('{prefix}{channel}{disable}{gain}{input_type}{bias}{srb1}{srb2}{suffix}'.format(
+      prefix = CytonCommands.CHANNEL_CONFIG_PREFIX.value,
+      channel = channel.value,
       disable = int(disable),
-      gain = gain,
-      input_type = input_type,
+      gain = gain.value,
+      input_type = input_type.value,
       bias = int(bias),
       srb1 = int(srb1),
-      srb2 = int(srb2)
+      srb2 = int(srb2),
+      suffix = CytonCommands.CHANNEL_CONFIG_SUFFIX.value
     ))
     return True
 
-  def _start_sd_recording(self) -> bool:
-    self._send_command('J')
+  def _start_sd_recording(self, duration: CytonRecordingDuration = CytonRecordingDuration.MIN_120) -> bool:
+    self._send_command(duration.value)
     return True
 
   def _stop_sd_recording(self) -> bool:
-    self._send_command('j')
+    self._send_command(CytonCommands.SD_STOP.value)
     return True
 
-  def _set_sample_rate(self, sample_rate: int = 6) -> bool:
-    """
-    0 = 16000 Hz
-    1 = 8000 Hz
-    2 = 4000 Hz
-    3 = 2000 Hz
-    4 = 1000 Hz
-    5 = 500 Hz
-    6 = 250 Hz
-    """
-    if sample_rate < 0 or sample_rate > 6:
-      raise ValueError('Invalid sample rate switch')
-    self._send_command('~{}'.format(sample_rate))
+  def _set_sample_rate(self, sample_rate: CytonSampleRate) -> bool:
+    self._send_command('{}{}'.format(CytonCommands.SAMPLE_RATE_PREFIX.value, sample_rate.value))
     return True
   
   def _start_time_stamping(self) -> bool:
-    self._send_command('<')
+    self._send_command(CytonCommands.TIMESTAMP_START.value)
     return True
 
   def _stop_time_stamping(self) -> bool:
-    self._send_command('>')
+    self._send_command(CytonCommands.TIMESTAMP_STOP.value)
     return True
     
   def _prepare_board(self) -> None:
