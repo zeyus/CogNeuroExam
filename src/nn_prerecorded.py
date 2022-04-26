3307eab4956e4dcfcaf783f09c961b7e4dd7d031
zeyus <zeyus@zeyus.com>
2022-04-27 00:12:28 +0200

    now apply NN training and validation per subject.

--- src/nn_prerecorded.py
+++ src/nn_prerecorded.py
@@ -11,16 +11,26 @@ dataset = ds_config.auto_construct_dataset()
 
 def make_model_and_process():
     tidnet = TIDNet.from_dataset(dataset)
-    return StandardClassification(tidnet, cuda=experiment.use_gpu, learning_rate=ds_config.lr)
+    return StandardClassification(tidnet, cuda=experiment.use_gpu, learning_rate=ds_config.lr, **experiment.architecture.as_dict())
 
-results = list()
-for training, validation, test in dataset.lmso(ds_config.train_params.folds):
+# results = list()
+for subject_name in dataset.get_thinkers():
+    print("Processiong subject: {}".format(subject_name))
+    thinker = dataset.thinkers[subject_name]
+    # note, "testing" is used for validation, as you can't set test fraction to 0.
+    training, _, testing = thinker.split(test_frac = ds_config.train_params.validation_fraction, validation_frac = 0)
     process = make_model_and_process()
+    process.fit(training_dataset=training, validation_dataset=testing, **ds_config.training_configuration.as_dict())
+    
 
-    process.fit(training_dataset=training, validation_dataset=validation, **ds_config.training_configuration.as_dict())
+# SPLIT BY SUBJECT, NOT IDEAL
+# for training, validation, test in dataset.lmso(ds_config.train_params.folds):
+#     process = make_model_and_process()
 
-    results.append(process.evaluate(test)['Accuracy'])
+#     process.fit(training_dataset=training, validation_dataset=validation, **ds_config.training_configuration.as_dict())
 
-print(results)
-print("Average accuracy: {:.2%}".format(sum(results)/len(results)))
+#     results.append(process.evaluate(test)['Accuracy'])
+
+# print(results)
+# print("Average accuracy: {:.2%}".format(sum(results)/len(results)))
 
