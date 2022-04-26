b2ad8b45db1dfa8cbe04de0ae3aaae321feb14ff
zeyus <zeyus@zeyus.com>
2022-04-26 08:19:15 +0200

    dn3 working example

--- src/nn_prerecorded.py
+++ src/nn_prerecorded.py
@@ -1,6 +1,38 @@
-from dn3.configuratron.config import ExperimentConfig
+from dn3.configuratron import ExperimentConfig
+from dn3.trainable.processes import StandardClassification
+from dn3.trainable.models import TIDNet
 
-experiment = ExperimentConfig("src/dn3_conf.yml")
+experiment = ExperimentConfig("./src/dn3_conf.yml")
+ds_config = experiment.datasets['clencher']
 
-dataset = experiment.datasets['public_dataset'].auto_construct_dataset()
-# Do some awesome things
+dataset = ds_config.auto_construct_dataset()
+
+
+def make_model_and_process():
+    tidnet = TIDNet.from_dataset(dataset)
+    return StandardClassification(tidnet, cuda=experiment.use_gpu, learning_rate=ds_config.lr)
+
+results = list()
+for training, validation, test in dataset.lmso(ds_config.train_params.folds):
+    process = make_model_and_process()
+
+    process.fit(training_dataset=training, validation_dataset=validation, **ds_config.training_configuration.as_dict())
+
+    results.append(process.evaluate(test)['Accuracy'])
+
+print(results)
+print("Average accuracy: {:.2%}".format(sum(results)/len(results)))
+
+# results = dict()
+# for training, validation, test in dataset.loso(ds_config.train_params.folds):
+#     process = make_model_and_process()
+
+#     process.fit(training_dataset=training, validation_dataset=validation,
+#                 epochs=ds_config.training_configuration.epochs,
+#                 batch_size=ds_config.training_configuration.batch_size)
+
+#     for _, _, test_thinker in test.loso():
+#         results[test_thinker.person_id] = process.evaluate(test_thinker)
+
+# print(results)
+# print("Average accuracy: {:.2%}".format(sum(results.values())/len(results)))
