7c8189e97474ca998b49da07ce3bbdad04152c5b
zeyus <zeyus@zeyus.com>
2022-04-26 22:50:52 +0200

    added changes

--- src/nn_prerecorded.py
+++ src/nn_prerecorded.py
@@ -7,6 +7,7 @@ ds_config = experiment.datasets['clencher']
 
 dataset = ds_config.auto_construct_dataset()
 
+# dataset.add_transform()
 
 def make_model_and_process():
     tidnet = TIDNet.from_dataset(dataset)
@@ -23,16 +24,3 @@ for training, validation, test in dataset.lmso(ds_config.train_params.folds):
 print(results)
 print("Average accuracy: {:.2%}".format(sum(results)/len(results)))
 
-# results = dict()
-# for training, validation, test in dataset.loso(ds_config.train_params.folds):
-#     process = make_model_and_process()
-
-#     process.fit(training_dataset=training, validation_dataset=validation,
-#                 epochs=ds_config.training_configuration.epochs,
-#                 batch_size=ds_config.training_configuration.batch_size)
-
-#     for _, _, test_thinker in test.loso():
-#         results[test_thinker.person_id] = process.evaluate(test_thinker)
-
-# print(results)
-# print("Average accuracy: {:.2%}".format(sum(results.values())/len(results)))
