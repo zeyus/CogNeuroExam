0078c94d2eedec13031b7132aa4df3c9560db395
zeyus <zeyus@zeyus.com>
2022-04-27 19:19:03 +0200

    added deep1010 scaling params

--- src/nn_prerecorded.py
+++ src/nn_prerecorded.py
@@ -1,13 +1,21 @@
 from dn3.configuratron import ExperimentConfig
 from dn3.trainable.processes import StandardClassification
 from dn3.trainable.models import TIDNet
+from dn3.data.utils import get_dataset_max_and_min
 
 
 experiment = ExperimentConfig("./src/dn3_conf.yml")
 ds_config = experiment.datasets['clencher']
 
 dataset = ds_config.auto_construct_dataset()
-
+if isinstance(ds_config.data_min, bool) or isinstance(ds_config.data_max, bool):
+    if experiment.experiment.get('deep1010', True):
+        print("Cannot calculate data range with deep1010 enabled, please set to false, calculate then set to true again.")
+        exit()
+    dr = get_dataset_max_and_min(dataset)
+    print("Calculated data range, please update dn3_conf.yml and rerun.")
+    print(dr)
+    exit()
 # dataset.add_transform()
 
 def make_model_and_process():
@@ -16,6 +24,8 @@ def make_model_and_process():
 
 # results = list()
 for subject_name in dataset.get_thinkers():
+    if not subject_name == 'l':
+        continue
     print("Processiong subject: {}".format(subject_name))
     thinker = dataset.thinkers[subject_name]
     # note, "testing" is used for validation, as you can't set test fraction to 0.
