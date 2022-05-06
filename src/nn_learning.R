d901b794060413e7f62ef186462f0839322bac45
zeyus <zeyus@zeyus.com>
2022-05-06 15:32:43 +0200

    new

--- src/nn_learning.R
+++ src/nn_learning.R
@@ -1,10 +1,10 @@
 library(tidyverse)
 
-df <- read.csv("output/details_a_2022-05-05_00-57-33_valid.csv")
+df <- read.csv("output/details_l_2022-05-05_10-22-12_valid.csv")
 
 head(df)
 
-df %>%
+df %>% filter(epoch > 5500) %>% filter(epoch < 5750) %>%
   ggplot(aes(x = epoch)) +
   geom_line(aes(y = loss), color = "red") +
   geom_line(aes(y = Accuracy), color = "blue") +
